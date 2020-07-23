a5d1ce5c662dabcff1377d13b57dc087f6afe44b
Max Kobrow <max.kobrow@gmail.com>
2020-07-23 13:51:06 +0200

    Added config_blanko.py

---
+++ config_blanko.py
@@ -0,0 +1,15 @@
+almaweb = {
+    "user": "<username for almaweb>",
+    "password": "<password for almaweb>"
+}
+email = {
+    "From": "<Sender Mail>",
+    "FromName": "<Sender Name>",
+    "FromPassword": "<Sender Password>",
+    "FromSMTP": "<Sender SMPT server adress>",
+    "To": "<mail the news should be delivered to>"
+}
+refreshTime = {
+    "upper": 1800,
+    "lower": 2700
+}
